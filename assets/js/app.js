dd2eb2179e05d75f66d1b5bb0a01c373c653141e
cynthia1171 <c.isla1171@gmail.com>
2018-02-21 21:38:16 -0300

    añadiendo imagenes y cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,6 +48,7 @@ function aparece(user) {
   var user = user;
   if (user.emailVerified) {
     $('.content').hide();
+    $('.perfil').hide();
     $('.inicio').show();
   }
 }
@@ -75,4 +76,14 @@ function verificar() {
     // An error happened.
     console.log(error);
   });
+}
+
+function pagPrincipal() {
+  $('.principal').show();
+  $('.perfil').hide();
+}
+
+function miPerfil() {
+  $('.principal').hide();
+  $('.perfil').show();
 }
